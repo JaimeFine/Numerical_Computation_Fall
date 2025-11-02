7d947b7e86c9b5ecdfda31ad4994ceda465ee3bc
JaimeFine <jaimefine6@gmail.com>
2025-11-02 17:35:29 +0800

    Main solve

--- Course_Work/assign_4/README.md
+++ Course_Work/assign_4/README.md
@@ -4,10 +4,10 @@ Find the smallest positive root of the nonlinear equation:
 
 $$\cos x + \frac{1}{1 + {e}^{-2x}} = 0$$
 
-Take the initial value $$\mathbf{x_{0} = 3}$$, and examine the following iterative schemes:
+Take the initial value $$x_{0} = 3$$, and examine the following iterative schemes:
 
 1. $$x_{k+1} = \arccos \left(-\frac{1}{1 + {e}^{-2x_{k}}} \right)$$ for $$k = 0, 1, 2, ...$$;
-2. $$\vcenter x_{k+1} = 0.5\ln \left(-\frac{1}{1+1 \ \cos x_{k}} \right)$$
+2. $$\vcenter x_{k+1} = 0.5\ln \left(-\frac{1}{1+1 \ \cos x_{k}} \right)$$;
 3. Netwon's method;
 
 For each of the above iterative schemes, first prove that it indeed corresponds to an equivalent fixed-point problem, then theoretically analyze whether it converges locally and what the rate of convergence is. Finally, implement the method and verify your conclusions.
---
+++ Course_Work/assign_4/assign_4.jl
@@ -0,0 +1,71 @@
+f(x) = cos(x) + 1.0 / (1.0 + exp(-2.0 * x))
+df(x) = -sin(x) + 2.0 * exp(-2.0 * x) / (1.0 + exp(-2.0 * x))^2
+
+function iteration_1(x0; max_iter=100, tol=1e-8)
+    x = x0
+    for i in 1:max_iter
+        x_new = acos(-1.0 / (1.0 + exp(-2 * x)))
+
+        if abs(x_new - x) < tol
+            println(
+                "The minimum positive root of the first scheme is: $x_new"
+            )
+            return x_new
+        end
+        x = x_new
+    end
+    println("Unable to solve after $max_iter iterations.")
+    return x_new
+end
+
+function iteration_2(x0; max_iter=100, tol=1e-8)
+    x = x0
+    for i in 1:max_iter
+        arg = -1.0 / (1.0 + 1.0 / cos(x))
+        if arg <= 0.0
+            println("Error! Log of non-positive number at x = $x.")
+            return nothing
+        end
+        
+        x_new = 0.5 * log(arg)
+
+        if abs(x_new - x) < tol
+            println(
+                "The minimum positive root of the second scheme is: $x_new"
+            )
+            return x_new
+        end
+        x = x_new
+    end
+    println("Unable to solve after $max_iter iterations.")
+    return x_new
+end
+
+function newton(f, df, x0; max_iter=100, tol=1e-8)
+    x = x0
+    for i in 1:max_iter
+        fx = f(x)
+        dfx = df(x)
+        if abs(dfx) < eps()
+            error("Derivative too small...")
+        end
+
+        x_new = x - fx / dfx
+
+        if abs(x_new - x) < tol
+            println(
+                "The minimum positive root of the newton's method is: $x_new"
+            )
+            return x_new
+        end
+        x = x_new
+    end
+    println("Unable to solve after $max_iter iterations.")
+    return x_new
+end
+
+iteration_1(3)
+println("-----------------------------------")
+iteration_2(3)
+println("-----------------------------------")
+newton(f, df, 3)
