d2a838779d727a54ebb5de51b806c2230216f945
JaimeFine <jaimefine6@gmail.com>
2025-11-23 09:43:45 +0800

    Half done

--- Course_Work/assign_6/assign_6.jl
+++ Course_Work/assign_6/assign_6.jl
@@ -1,1 +1,28 @@
-# assign_6...
+x = [1.0, 2.0, 3.0, 4.0]
+y = [60.0, 30.0, 20.0, 15.0]
+# Transform it into linear equation:
+# ln(y) = bx + ln(a)
+y_new = log.(y)
+
+function build_matrix(x)
+    n = length(x)
+    X = Array{Float64}(undef, n, 2)
+    for i in 1:n
+        X[i, 1] = 1.0
+        X[i, 2] = x[i]
+    end
+    return X
+end
+
+X = build_matrix(x)
+
+Xt = transpose(X)
+XtX = Xt * X
+Xty = Xt * y_new
+
+XtX_inv = inv(XtX)
+result = XtX_inv * Xt * y_new
+b = result[2]
+a = exp(result[1])
+
+println("a is ", a, ", b is ", b)
