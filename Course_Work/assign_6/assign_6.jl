04db7ce7e7ce0b9ce13f93f27e505acac0bd9dd5
JaimeFine <jaimefine6@gmail.com>
2025-11-23 10:10:43 +0800

    Done

--- Course_Work/assign_6/assign_6.jl
+++ Course_Work/assign_6/assign_6.jl
@@ -1,3 +1,5 @@
+using Plots
+
 x = [1.0, 2.0, 3.0, 4.0]
 y = [60.0, 30.0, 20.0, 15.0]
 # Transform it into linear equation:
@@ -26,3 +28,8 @@ b = result[2]
 a = exp(result[1])
 
 println("a is ", a, ", b is ", b)
+
+x_r = -10.0:0.01:20.0
+y_r = [a * exp(b * xi) for xi in x_r]
+
+plot(x_r, y_r)
