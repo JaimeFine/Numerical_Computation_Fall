e6cfccd58782d7a11aaeae5446fe11f06b7ab292
JaimeFine <jaimefine6@gmail.com>
2025-12-02 21:18:58 +0800

    Update

--- Course_Work/assign_7/assign_7.jl
+++ Course_Work/assign_7/assign_7.jl
@@ -260,5 +260,5 @@ r, θ = rodrigues_from_R(R)
 # Panning: x, y, z = -15.174821832828904   9.590334711153755  21.176335086376653
 
 rot = r * 180 / pi
-println("Rodriguez's vecotr r: ", rot)
+println("Rodriguez's vector r: ", rot)
 println("Translation t: ", vec(t))
