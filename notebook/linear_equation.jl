341ef98df756da3a20e0a1c0ea7e56c4527753ed
JaimeFine <jaimefine6@gmail.com>
2025-12-15 21:59:02 +0800

    Half done

---
+++ notebook/linear_equation.jl
@@ -0,0 +1,194 @@
+### A Pluto.jl notebook ###
+# v0.20.21
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 61958f32-a8b6-40d6-be2e-858574ab7708
+begin
+	using LinearAlgebra
+
+	# Declare matrix A and vector b, Ax = b
+	A = [10.0 -1.0 -2.0;
+		 -1.0 10.0 -2.0;
+		 -1.0 -1.0 5.0]
+	
+	b = [7.2, 8.3, 4.2]
+	
+	function jacobi(
+		A::Matrix{Float64}, b::Vector{Float64};
+		x=zeros(size(b)), tol=1e-8, max_iter=100
+	)
+		n = length(b)
+		D = Diagonal(diag(A))
+		
+		# The following can be written as:
+		# L = tril(A, -1)
+		# U = triu(A, 1)
+		# But just for knowing how we get it...
+		L = LowerTriangular(A) - D
+		U = UpperTriangular(A) - D
+	
+		D_inv = inv(D)
+	
+		for k in 1:max_iter
+			x_new = -D_inv * (L + U) * x + D_inv * b
+			if norm(x_new - x, Inf) < tol
+				println("Approximation: ", x_new)
+				return x_new
+			end
+			x = x_new
+		end
+		
+		return x
+	end
+
+	jacobi(A, b)
+end
+
+# ╔═╡ 6ca1508e-d9b3-11f0-85e3-a9cca3c67ff7
+md"""
+# Numerical methods for Linear Equations
+
+This is a notebook about numerical linear algebra algorithms in numerical computation!
+
+## Example
+
+An example with function
+
+$\begin{cases}
+10x_1 - x_2 - 2x_3 = 7.2 \\
+-x_1 + 10x_2 - 2x_3 = 8.3 \\
+-x_1 - x_2 + 5x_3 = 4.2
+\end{cases}$
+
+### Jacobi Method
+"""
+
+# ╔═╡ f880da5a-0dd1-4592-bf18-3d830441abd4
+md"""
+### Gauss-Seidel Method
+"""
+
+# ╔═╡ 41c19325-2ac9-42f7-862e-37abd505bfc8
+begin
+	function gauss_seidel(
+		A::Matrix{Float64}, b::Vector{Float64};
+		x=zeros(size(b)), tol=1e-8, max_iter=100
+	)
+		n = length(b)
+		D = Diagonal(diag(A))
+		L = LowerTriangular(A) - D
+		U = UpperTriangular(A) - D
+
+		DL_inv = inv(D + L)
+
+		for k in 1:max_iter
+			x_new = DL_inv * (b - U * x)
+			if norm(x_new - x, Inf) < tol
+				println("Approximation: ", x_new)
+				return x_new
+			end
+			x = x_new
+		end
+
+		return x
+	end
+
+	# A and b was already declared previously
+	gauss_seidel(A, b)
+end
+
+# ╔═╡ ce647298-2958-48fa-9361-3f48955a098f
+md"""
+### Successive Over Relaxation (SOR)
+"""
+
+# ╔═╡ 317227db-1cf5-4329-8441-9ad00a9b1013
+begin
+	function sor(
+		A::Matrix{Float64}, b::Vector{Float64}, ω;
+		x=zeros(size(b)), tol=1e-8, max_iter=100
+	)
+		n = length(b)
+		I = ones(size(b))
+		D = Diagonal(diag(A))
+		L = LowerTriangular(A) - D
+		U = UpperTriangular(A) - D
+
+		M = D + ω * L
+		N = (1 - ω) * D - ω * U
+		B = inv(M) * N
+		g = ω * inv(M) * b
+
+		for k in 1:max_iter
+			x_new = B * x + g 
+			if norm(x_new - x, Inf) < tol
+				println("Approximation: ", x_new)
+				return x_new
+			end
+			x = x_new
+		end
+
+		return x
+	end
+
+	# Define the relaxation parameter:
+	ω = 1.1
+	
+	# A and b was already declared previously...
+	sor(A, b, ω)
+end
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+LinearAlgebra = "37e2e46d-f89d-539d-b4ee-838fcccc9c8e"
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.12.2"
+manifest_format = "2.0"
+project_hash = "f352ceee806168c8ae38887a01d7bae6ca62470b"
+
+[[deps.Artifacts]]
+uuid = "56f22d72-fd6d-98f1-02f0-08ddc0907c33"
+version = "1.11.0"
+
+[[deps.CompilerSupportLibraries_jll]]
+deps = ["Artifacts", "Libdl"]
+uuid = "e66e0078-7015-5450-92f7-15fbd957f2ae"
+version = "1.3.0+1"
+
+[[deps.Libdl]]
+uuid = "8f399da3-3557-5675-b5ff-fb832c97cbdb"
+version = "1.11.0"
+
+[[deps.LinearAlgebra]]
+deps = ["Libdl", "OpenBLAS_jll", "libblastrampoline_jll"]
+uuid = "37e2e46d-f89d-539d-b4ee-838fcccc9c8e"
+version = "1.12.0"
+
+[[deps.OpenBLAS_jll]]
+deps = ["Artifacts", "CompilerSupportLibraries_jll", "Libdl"]
+uuid = "4536629a-c528-5b80-bd46-f80d51c5b363"
+version = "0.3.29+0"
+
+[[deps.libblastrampoline_jll]]
+deps = ["Artifacts", "Libdl"]
+uuid = "8e850b90-86db-534c-a0d3-1478176c7d93"
+version = "5.15.0+0"
+"""
+
+# ╔═╡ Cell order:
+# ╠═6ca1508e-d9b3-11f0-85e3-a9cca3c67ff7
+# ╠═61958f32-a8b6-40d6-be2e-858574ab7708
+# ╟─f880da5a-0dd1-4592-bf18-3d830441abd4
+# ╠═41c19325-2ac9-42f7-862e-37abd505bfc8
+# ╟─ce647298-2958-48fa-9361-3f48955a098f
+# ╠═317227db-1cf5-4329-8441-9ad00a9b1013
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
