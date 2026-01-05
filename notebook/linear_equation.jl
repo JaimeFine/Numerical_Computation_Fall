5bd410bd981e8a56e4b2b113765137fc85f6ba90
Jaime Fan <jaimefine6@gmail.com>
2026-01-05 19:05:55 +0800

    Update linear_equation.jl

--- notebook/linear_equation.jl
+++ notebook/linear_equation.jl
@@ -432,49 +432,6 @@ begin
 	chasing_method(l, d, u, c)
 end
 
-# ╔═╡ 00000000-0000-0000-0000-000000000001
-PLUTO_PROJECT_TOML_CONTENTS = """
-[deps]
-LinearAlgebra = "37e2e46d-f89d-539d-b4ee-838fcccc9c8e"
-"""
-
-# ╔═╡ 00000000-0000-0000-0000-000000000002
-PLUTO_MANIFEST_TOML_CONTENTS = """
-# This file is machine-generated - editing it directly is not advised
-
-julia_version = "1.12.2"
-manifest_format = "2.0"
-project_hash = "f352ceee806168c8ae38887a01d7bae6ca62470b"
-
-[[deps.Artifacts]]
-uuid = "56f22d72-fd6d-98f1-02f0-08ddc0907c33"
-version = "1.11.0"
-
-[[deps.CompilerSupportLibraries_jll]]
-deps = ["Artifacts", "Libdl"]
-uuid = "e66e0078-7015-5450-92f7-15fbd957f2ae"
-version = "1.3.0+1"
-
-[[deps.Libdl]]
-uuid = "8f399da3-3557-5675-b5ff-fb832c97cbdb"
-version = "1.11.0"
-
-[[deps.LinearAlgebra]]
-deps = ["Libdl", "OpenBLAS_jll", "libblastrampoline_jll"]
-uuid = "37e2e46d-f89d-539d-b4ee-838fcccc9c8e"
-version = "1.12.0"
-
-[[deps.OpenBLAS_jll]]
-deps = ["Artifacts", "CompilerSupportLibraries_jll", "Libdl"]
-uuid = "4536629a-c528-5b80-bd46-f80d51c5b363"
-version = "0.3.29+0"
-
-[[deps.libblastrampoline_jll]]
-deps = ["Artifacts", "Libdl"]
-uuid = "8e850b90-86db-534c-a0d3-1478176c7d93"
-version = "5.15.0+0"
-"""
-
 # ╔═╡ Cell order:
 # ╠═6ca1508e-d9b3-11f0-85e3-a9cca3c67ff7
 # ╠═61958f32-a8b6-40d6-be2e-858574ab7708
@@ -494,5 +451,3 @@ version = "5.15.0+0"
 # ╠═00d80502-d7af-4382-85e5-065e695b7eb4
 # ╟─ff12925c-8bc5-499d-bb2a-ec12713fabd0
 # ╠═6f04dae6-a74a-448d-8fb8-32f612c094e1
-# ╟─00000000-0000-0000-0000-000000000001
-# ╟─00000000-0000-0000-0000-000000000002
